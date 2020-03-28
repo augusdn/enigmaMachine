105272c240baaf50ea093e9b025fa5ad8ace6716
Augustine <alee951214@gmail.com>
2020-03-29 06:21:22 +1100

    laptop job

--- Plugboard.py
+++ Plugboard.py
@@ -1,21 +1,24 @@
 class PlugBoard:
     def __init__(self):
-        self._settings = list(range(26))
+        self._settings = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
     
-    def translate(self, number):
+    def translate(self, c):
+        number = ord(c)-65
         result = self._settings[number]
         return result
 
-    def connectTwo(self, t1, t2):
-        if self._settings[t1] != t1:
+    def connectTwo(self, c1, c2):
+        t1 = ord(c1)-65
+        t2 = ord(c2)-65
+        if self._settings[t1] != c1:
             # print(chr(t1+65) + " is already in use")
             return -1
-        if self._settings[t2] != t2:
+        if self._settings[t2] != c2:
             # print(chr(t2+65) + " is already in use")
             return -2
         else:
-            self._settings[t1] = t2
-            self._settings[t2] = t1
+            self._settings[t1] = c2
+            self._settings[t2] = c1
             return 0
     
     def removeTwo(self, t1, t2):
--- Rotor.py
+++ Rotor.py
@@ -1,8 +1,18 @@
 class Rotor:
     def __init__(self, wiring, notch, initial_position, ring_setting):
-        self._wiring = []
-        self._notch = None
-        self._position = 0
-        self._ring = 0
+        self._wiring = wiring
+        self._notch = ord(notch.upper())-65
+        self._position = ord(initial_position.upper())-65
+        self._ring = ord(ring_setting.upper())-65
 
     def rotate(self):
+        self._position = (self._position + 1) % 26
+        if self._position == self._notch + 1:
+            return 1
+        else:
+            return 0
+
+    def translate(self, t1):
+        result = self._wiring[t1]
+        print (result)
+        return result
